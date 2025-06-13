2d76d604602c4aaaa6a5f286dd1ae4fe16fe7325
SaiMaiNow <commanchannel@gmail.com>
2025-06-13 12:24:22 +0700

    เพิ่มฟังก์ชันการจัดการไฟล์ด้วย multer และปรับปรุงเส้นทางการอัปโหลดใน API สำหรับการสร้างและอัปเดตผลิตภัณฑ์

--- backend/routes/router/products.js
+++ backend/routes/router/products.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 const sqlite3 = require('../../functions/sqlite3');
 
+const { upload, deleteFile } = require('../../functions/storage');
+
 router.get('/get-all', async (req, res) => {
     try {
         const db = await sqlite3.connectDatabase();
@@ -16,7 +18,7 @@ router.get('/get-all', async (req, res) => {
             if (!rows || rows.length === 0) {
                 return res.status(404).json({ error: 'No products found' });
             }
-            
+
             return res.status(200).json(rows);
         });
     } catch (error) {
@@ -25,7 +27,10 @@ router.get('/get-all', async (req, res) => {
     }
 });
 
-router.post('/create', async (req, res) => {
+router.post('/create', [upload.fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'file', maxCount: 1 }
+])], async (req, res) => {
     try {
         const { name, price, description } = req.body;
         const files = req.files;
@@ -42,7 +47,7 @@ router.post('/create', async (req, res) => {
         const token = uuidv4();
 
         db.run(`INSERT INTO products (name, price, description, image, file, token) VALUES (?, ?, ?, ?, ?, ?)`,
-            [name, price, description, files.image[0].path, files.file[0].path, token],
+            [name, price, description, files.image[0].fileUrl, files.file[0].fileUrl, token],
             function (err) {
                 if (err) {
                     throw new Error(err);
@@ -50,42 +55,59 @@ router.post('/create', async (req, res) => {
             }
         );
 
-        res.status(201).json({ message: 'Product created successfully', product: {
-            name, price, description, image: files.image[0].path, file: files.file[0].path, token
-        }});
+        res.status(201).json({
+            message: 'Product created successfully', product: {
+                name, price, description, image: files.image[0].fileUrl, file: files.file[0].fileUrl, token
+            }
+        });
     } catch (err) {
         console.error('Error creating product:', err);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-router.put('/update/:token', async (req, res) => {
+router.put('/update/:token', [upload.fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'file', maxCount: 1 }
+])], async (req, res) => {
     try {
         const { token } = req.params;
         const { name, price, description } = req.body;
         const files = req.files;
-
-        if (!files || !files.image || !files.file) {
-            return res.status(400).json({ error: 'Image and file are required' });
-        }
 
         if (!name || !price || !description) {
             return res.status(400).json({ error: 'All fields are required' });
         }
 
         const db = await sqlite3.getDatabase();
-        db.run(`UPDATE products SET name = ?, price = ?, description = ?, image = ?, file = ? WHERE token = ?`,
-            [name, price, description, files.image[0].path, files.file[0].path, token],
-            function (err) {
+        db.run(`UPDATE products SET name = ?, price = ?, description = ? WHERE token = ?`,
+            [name, price, description, token],
+            function (err, rows) {
+                if (rows.length === 0) {
+                    return res.status(404).json({ error: 'Product not found' });
+                }
+
                 if (err) {
                     throw new Error(err);
+                }
+
+                if (files && files.image && files.image[0]) {
+                    deleteFile(rows[0].image);
+                    db.run(`UPDATE products SET image = ? WHERE token = ?`, [files.image[0].fileUrl, token]);
+                }
+
+                if (files && files.file && files.file[0]) {
+                    deleteFile(rows[0].file);
+                    db.run(`UPDATE products SET file = ? WHERE token = ?`, [files.file[0].fileUrl, token]);
                 }
             }
         );
 
-        res.status(200).json({ message: 'Product updated successfully', product: {
-            id, name, price, description, image: files.image[0].path, file: files.file[0].path, token
-        }});
+        res.status(200).json({
+            message: 'Product updated successfully', product: {
+                id, name, price, description, image: files.image[0].fileUrl, file: files.file[0].fileUrl, token
+            }
+        });
     } catch (err) {
         console.error('Error updating product:', err);
         res.status(500).json({ error: 'Internal Server Error' });
