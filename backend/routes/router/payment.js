a97fb0f2621cf185ab6092e6a1bc9765eadbaa92
SaiMaiNow <commanchannel@gmail.com>
2025-06-15 11:37:00 +0700

    Configure CORS to allow requests from localhost:5500 Implement skeleton for get-payment route with commented-out logic

--- backend/routes/router/payment.js
+++ backend/routes/router/payment.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/get-payment', (req, res) => {});
+router.get('/get-payment', (req, res) => {
+    // try {
+    //     const username = req.session.user.username;
+    //     if (!username) {
+    //         return res.status(404).json({ ok: false, message: 'user is not login' });
+    //     }
+
+    //     res.status(200).json({ ok: true,  });
+    // } catch (err) {
+    //     console.error('Get payment / :', err);
+    //     res.status(500).json({ ok: false, message: 'Internal server error' });
+    // }
+});
 
 module.exports = router;
