a72100ba9abe7af61d299797a61631f49a06e412
pppskk <namthip_m@cmu.ac.th>
2025-06-17 15:31:25 +0700

    Add product upload form และ JavaScript เชื่อม api create products

---
+++ scripts/products.js
@@ -0,0 +1,90 @@
+const ajax = async (config) => {
+  const request = await fetch(config.url, {
+    method: config.method,
+    credentials: "include",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(config.data),
+  });
+  const response = await request.json();
+  return response;
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+  const auth = await ajax({
+    url: "http://localhost:4000/api/signin/check",
+    method: "GET",
+  });
+
+  if (!auth.ok) {
+    window.location.href = "signin.html";
+    return;
+  }
+
+  console.log("User authenticated:", auth.user);
+
+
+  const form = document.querySelector(".product-form");
+  form.classList.remove("d-none");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const title = e.target.title.value.trim();
+    const description = e.target.description.value.trim();
+    const price = e.target.price.value.trim();
+    const subject = e.target.subject.value;
+    const file = e.target.file.files[0];
+    const image = e.target.image.files[0];
+
+    const formData = new FormData();
+    formData.append("name", title);
+    formData.append("description", description);
+    formData.append("price", price);
+    formData.append("file", file); 
+    formData.append("image", image); 
+
+    console.log(e.target.file.files[0]);
+    console.log(e.target.image.files[0]);
+    console.log({
+      title,
+      description,
+      price,
+      subject,
+      fileName: file.name,
+      imageName: image.name
+    });
+    try {
+      const res = await fetch("http://localhost:4000/api/products/create", {
+        method: "POST",
+        credentials: "include",
+        body: formData
+      });
+
+      const result = await res.json();
+
+      if(!result.ok) {
+        Swal.fire({
+          icon: "error",
+          title: "Upload failed!",
+          text: response.message || "Something went wrong!"
+        });
+        return;
+      }
+      Swal.fire({
+        icon: "success",
+        title: "Upload success!",
+        text: response.message,
+        showConfirmButton: false,
+        timer: 1000
+      })
+
+    } catch (err) {
+      console.error("Upload error:", err);
+      alert("An error occurred while uploading the product. Please try again.");
+    }
+  });
+});
